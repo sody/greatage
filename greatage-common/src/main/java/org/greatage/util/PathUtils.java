6c5c54c1245c38b7ac5b3d5d372effaa5a2a97cc
Ivan Khalopik <ikhalopik@gmail.com>
2011-05-21 13:06:19 +0300

    greatage-25: Reorganize ResourceLocator service

---
+++ greatage-common/src/main/java/org/greatage/util/PathUtils.java
@@ -0,0 +1,278 @@
+/*
+ * Copyright (c) 2008-2011 Ivan Khalopik.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.greatage.util;
+
+import java.io.File;
+import java.util.Set;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * @author Ivan Khalopik
+ * @since 1.0
+ */
+public abstract class PathUtils {
+	private static final Pattern ANT_TEXT_PATTERN = Pattern.compile("\\?|\\*");
+
+	private static final String PATH_SEPARATOR = "/";
+
+	private static final String ANT_ANY_PATH_MARKER = "**";
+	private static final String ANT_ANY_STRING_MARKER = "*";
+	private static final String ANT_ANY_CHARACTER_MARKER = "?";
+
+	/**
+	 * Looks up for all resources under given path which must be matched against include and exclude patterns. If there
+	 * are no patterns specified, all resources found under that path will be returned.
+	 *
+	 * @param path	 path where to search child resources
+	 * @param includes set of include patterns, can be <code>null</code>
+	 * @param excludes set of exclude patterns, can be <code>null</code>
+	 * @return a set of all found resources which match include and exclude patterns or empty set if nothing found
+	 */
+	public static Set<String> findResources(final String path, final Set<String> includes, final Set<String> excludes) {
+		final String normalizedPath = normalizePath(path);
+		final File baseDir = new File(normalizedPath);
+		if (baseDir.exists() && baseDir.isDirectory()) {
+			final Set<String> realIncludes = includes != null ? includes : CollectionUtils.<String>newSet();
+			final Set<String> realExcludes = excludes != null ? excludes : CollectionUtils.<String>newSet();
+
+			return findResourcesInDirectory(baseDir, "", realIncludes, realExcludes);
+		}
+		return CollectionUtils.newSet();
+	}
+
+	/**
+	 * Normalizes given path by replacing file-dependent path separators like <code>'\'</code> with standard
+	 * <code>'/'</code>.
+	 *
+	 * @param path path which must be normalized, not <code>null</code>
+	 * @return normalized path, not <code>null</code>
+	 */
+	public static String normalizePath(final String path) {
+		return path.replaceAll("\\\\", PATH_SEPARATOR);
+	}
+
+	/**
+	 * Tests whether a given path matches include and exclude patterns. Include and exclude patterns may contain some
+	 * special characters:
+	 * <br/> '**' means any path
+	 * <br/> '*' means zero or more characters
+	 * <br/> '?' means one and only one character
+	 *
+	 * @param path	 path which must be matched against the patterns, not <code>null</code>
+	 * @param includes set of include patterns, not <code>null</code>
+	 * @param excludes set of exclude patterns, not <code>null</code>
+	 * @return <code>true</code> if path matches against include and exclude patterns, <code>false</code> otherwise
+	 */
+	public static boolean matchAntPath(final String path, final Set<String> includes, final Set<String> excludes) {
+		return (includes.isEmpty() || matchAntPath(path, includes)) && !matchAntPath(path, excludes);
+	}
+
+	private static boolean matchAntPath(final String path, final Set<String> includes) {
+		for (String include : includes) {
+			if (PathUtils.matchAntPath(path, include)) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	/**
+	 * Tests whether a given path matches against ant-style pattern. The pattern may contain some special characters:
+	 * <br/> '**' means any path
+	 * <br/> '*' means zero or more characters
+	 * <br/> '?' means one and only one character
+	 *
+	 * @param path	path which must be matched against the pattern, not <code>null</code>
+	 * @param pattern ant-style pattern to match against, not <code>null</code>
+	 * @return <code>true</code> if path matches against the pattern, <code>false</code> otherwise
+	 */
+	public static boolean matchAntPath(final String path, final String pattern) {
+		// When path starts with a /, pattern also has to start with a / and vice versa
+		if (path.startsWith(PATH_SEPARATOR) != pattern.startsWith(PATH_SEPARATOR)) {
+			return false;
+		}
+
+		final String[] patternDirs = pattern.split(PATH_SEPARATOR);
+		final String[] pathDirs = path.split(PATH_SEPARATOR);
+
+		int patternStart = 0;
+		int patternEnd = patternDirs.length - 1;
+		int pathStart = 0;
+		int pathEnd = pathDirs.length - 1;
+
+		// up to first '**'
+		while (patternStart <= patternEnd && pathStart <= pathEnd) {
+			if (patternDirs[patternStart].equals(ANT_ANY_PATH_MARKER)) {
+				break;
+			}
+			if (!matchAntPattern(pathDirs[pathStart], patternDirs[patternStart])) {
+				return false;
+			}
+			patternStart++;
+			pathStart++;
+		}
+		if (pathStart > pathEnd) {
+			// String is exhausted
+			for (int i = patternStart; i <= patternEnd; i++) {
+				if (!patternDirs[i].equals(ANT_ANY_PATH_MARKER)) {
+					return false;
+				}
+			}
+			return true;
+		} else if (patternStart > patternEnd) {
+			// String not exhausted, but pattern is. Failure.
+			return false;
+		}
+
+		// up to last '**'
+		while (patternStart <= patternEnd && pathStart <= pathEnd) {
+			if (patternDirs[patternEnd].equals(ANT_ANY_PATH_MARKER)) {
+				break;
+			}
+			if (!matchAntPattern(pathDirs[pathEnd], patternDirs[patternEnd])) {
+				return false;
+			}
+			patternEnd--;
+			pathEnd--;
+		}
+		if (pathStart > pathEnd) {
+			// String is exhausted
+			for (int i = patternStart; i <= patternEnd; i++) {
+				if (!patternDirs[i].equals(ANT_ANY_PATH_MARKER)) {
+					return false;
+				}
+			}
+			return true;
+		}
+
+		while (patternStart != patternEnd && pathStart <= pathEnd) {
+			int subPatternEnd = -1;
+			for (int i = patternStart + 1; i <= patternEnd; i++) {
+				if (patternDirs[i].equals(ANT_ANY_PATH_MARKER)) {
+					subPatternEnd = i;
+					break;
+				}
+			}
+			if (subPatternEnd == patternStart + 1) {
+				// '**/**' situation, so skip one
+				patternStart++;
+				continue;
+			}
+			// Find the pattern between padIdxStart & padIdxTmp in path between
+			// pathStart & pathEnd
+			final int patternLength = (subPatternEnd - patternStart - 1);
+			final int pathLength = (pathEnd - pathStart + 1);
+			int foundPosition = -1;
+
+			strLoop:
+			for (int i = 0; i <= pathLength - patternLength; i++) {
+				for (int j = 0; j < patternLength; j++) {
+					if (!matchAntPattern(pathDirs[pathStart + i + j], patternDirs[patternStart + j + 1])) {
+						continue strLoop;
+					}
+				}
+
+				foundPosition = pathStart + i;
+				break;
+			}
+			if (foundPosition == -1) {
+				return false;
+			}
+
+			patternStart = subPatternEnd;
+			pathStart = foundPosition + patternLength;
+		}
+
+		for (int i = patternStart; i <= patternEnd; i++) {
+			if (!patternDirs[i].equals(ANT_ANY_PATH_MARKER)) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+	/**
+	 * Tests whether a given string matches against a pattern. The pattern may contain two special characters:
+	 * <br/> '*' means zero or more characters
+	 * <br/> '?' means one and only one character
+	 *
+	 * @param text	string which must be matched against the pattern, not <code>null</code>
+	 * @param pattern pattern to match against, not <code>null</code>
+	 * @return <code>true</code> if the string matches against the pattern, <code>false</code> otherwise
+	 */
+	public static boolean matchAntPattern(final String text, final String pattern) {
+		final StringBuilder patternBuilder = new StringBuilder();
+		final Matcher matcher = ANT_TEXT_PATTERN.matcher(pattern);
+		int position = 0;
+		while (matcher.find()) {
+			patternBuilder.append(quote(pattern, position, matcher.start()));
+			final String match = matcher.group();
+			if (ANT_ANY_CHARACTER_MARKER.equals(match)) {
+				patternBuilder.append('.');
+			} else if (ANT_ANY_STRING_MARKER.equals(match)) {
+				patternBuilder.append(".*");
+			}
+			position = matcher.end();
+		}
+		patternBuilder.append(quote(pattern, position, pattern.length()));
+		return Pattern.compile(patternBuilder.toString()).matcher(text).matches();
+	}
+
+	/**
+	 * Quotes text in regexp pattern from specified start to end positions.
+	 *
+	 * @param text  text to be quoted
+	 * @param start start position
+	 * @param end   end position
+	 * @return regexp pattern with quoted text or empty string
+	 */
+	private static String quote(final String text, final int start, final int end) {
+		if (start >= end) {
+			return "";
+		}
+		return Pattern.quote(text.substring(start, end));
+	}
+
+	/**
+	 * Looks up for all resources under given directory which must be matched against include and exclude patterns.
+	 * Specified relative path will be added to resulting resource names.
+	 *
+	 * @param directory	directory where to search child resources, should exist and be directory, not <code>null</code>
+	 * @param relativePath relative path that will be added to resulting resource names, not <code>null</code>
+	 * @param includes	 set of include patterns, not <code>null</code>
+	 * @param excludes	 set of exclude patterns, not <code>null</code>
+	 * @return a set of all found resources which match include and exclude patterns or empty set if nothing found
+	 */
+	private static Set<String> findResourcesInDirectory(final File directory,
+														final String relativePath,
+														final Set<String> includes,
+														final Set<String> excludes) {
+		final Set<String> resources = CollectionUtils.newSet();
+		for (String fileName : directory.list()) {
+			final File child = new File(directory, fileName);
+			final String filePath = relativePath + fileName;
+			if (child.isDirectory()) {
+				resources.addAll(findResourcesInDirectory(child, filePath + PATH_SEPARATOR, includes, excludes));
+			} else if (matchAntPath(filePath, includes, excludes)) {
+				resources.add(filePath);
+			}
+		}
+		return resources;
+	}
+
+}
--- greatage-common/src/test/java/org/greatage/util/TestReflectionUtils.java
+++ greatage-common/src/test/java/org/greatage/util/TestReflectionUtils.java
@@ -134,4 +134,47 @@ public class TestReflectionUtils extends Assert {
 		final T actual = ReflectionUtils.findException(exception, exceptionClass);
 		assertEquals(actual, expected);
 	}
+
+	public static class MockClass {
+		private final String parameter1;
+		private final boolean parameter2;
+		private final CharSequence parameter3;
+
+		public MockClass() {
+			this(null);
+		}
+
+		public MockClass(final String parameter1) {
+			this(parameter1, false);
+		}
+
+		public MockClass(final String parameter1, final boolean parameter2) {
+			this(parameter1, parameter2, (CharSequence) null);
+		}
+
+		private MockClass(final String parameter1, final String parameter3) {
+			this(parameter1, false, (CharSequence) parameter3);
+		}
+
+		public MockClass(final String parameter1, final boolean parameter2, final String parameter3) {
+			throw new IllegalStateException();
+		}
+
+		public MockClass(final String parameter1, final boolean parameter2, final CharSequence parameter3) {
+			this.parameter1 = parameter1;
+			this.parameter2 = parameter2;
+			this.parameter3 = parameter3;
+		}
+
+		@Override
+		public boolean equals(Object o) {
+			if (this == o) return true;
+			if (o == null || getClass() != o.getClass()) return false;
+
+			final MockClass mockClass = (MockClass) o;
+			return parameter2 == mockClass.parameter2 &&
+					!(parameter1 != null ? !parameter1.equals(mockClass.parameter1) : mockClass.parameter1 != null) &&
+					!(parameter3 != null ? !parameter3.equals(mockClass.parameter3) : mockClass.parameter3 != null);
+		}
+	}
 }
