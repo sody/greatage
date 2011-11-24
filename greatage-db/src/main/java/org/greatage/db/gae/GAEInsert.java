3bceadc9351622cbb8bb21525af6d47874180dd0
Ivan Khalopik <ikhalopik@gmail.com>
2011-11-24 17:37:31 +0300

    greatage-db: gae batch insert implemented

--- greatage-db/src/main/java/org/greatage/db/gae/GAEInsert.java
+++ greatage-db/src/main/java/org/greatage/db/gae/GAEInsert.java
@@ -7,6 +7,7 @@ import org.greatage.db.Trick;
 import org.greatage.util.DescriptionBuilder;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -18,6 +19,8 @@ import java.util.Map;
 public class GAEInsert extends GAEChange implements Trick.Insert {
 	private final Entity entity;
 	private final Map<String, Trick.Select> setters = new HashMap<String, Trick.Select>();
+	private final List<String> into = new ArrayList<String>();
+	private final List<Entity> values = new ArrayList<Entity>();
 
 	GAEInsert(final String entityName) {
 		entity = new Entity(entityName);
@@ -34,11 +37,16 @@ public class GAEInsert extends GAEChange implements Trick.Insert {
 	}
 
 	public Trick.Insert into(final String... propertyNames) {
-		return null;  //To change body of implemented methods use File | Settings | File Templates.
+		into.addAll(Arrays.asList(propertyNames));
+		return this;
 	}
 
 	public Trick.Insert values(final Object... values) {
-		return null;  //To change body of implemented methods use File | Settings | File Templates.
+		final Entity value = new Entity(entity.getKind());
+		for (int i = 0; i < into.size(); i++) {
+			value.setProperty(into.get(i), values[i]);
+		}
+		return this;
 	}
 
 	public void doInDataStore(final DatastoreService dataStore) {
@@ -55,8 +63,14 @@ public class GAEInsert extends GAEChange implements Trick.Insert {
 				//todo: implement this
 			}
 		}
-
-		dataStore.put(entity);
+		if (values.isEmpty()) {
+			dataStore.put(entity);
+		} else {
+			for (Entity value : values) {
+				value.setPropertiesFrom(entity);
+			}
+			dataStore.put(values);
+		}
 	}
 
 	@Override
@@ -68,6 +82,19 @@ public class GAEInsert extends GAEChange implements Trick.Insert {
 		}
 		for (Map.Entry<String, Trick.Select> entry : setters.entrySet()) {
 			builder.append(entry.getKey(), entry.getValue());
+		}
+		if (!values.isEmpty()) {
+			builder.append("into", into);
+			for (Entity value : values) {
+				final StringBuilder sb = new StringBuilder();
+				for (String propertyName : into) {
+					if (sb.length() > 0) {
+						sb.append(',');
+					}
+					sb.append(value.getProperty(propertyName));
+				}
+				builder.append("values", sb);
+			}
 		}
 		return builder.toString();
 	}
