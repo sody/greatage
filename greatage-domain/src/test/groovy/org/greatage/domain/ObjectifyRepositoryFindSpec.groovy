6ce2fe40e056e71bdff4a06cb25366729c90ca18
Ivan Khalopik <ikhalopik@gmail.com>
2012-04-18 16:54:07 +0300

    issue #6: Domain API
    more objectify repository tests

--- greatage-domain/src/test/groovy/org/greatage/domain/ObjectifyRepositoryFindSpec.groovy
+++ greatage-domain/src/test/groovy/org/greatage/domain/ObjectifyRepositoryFindSpec.groovy
@@ -31,12 +31,12 @@ class ObjectifyRepositoryFindSpec extends Specification {
 		def objectifyFactory = new ObjectifyFactory()
 		objectifyFactory.register(CompanyImpl.class)
 		objectifyFactory.begin().put([
-				new CompanyImpl(id: 1l, name: "company1"),
-				new CompanyImpl(id: 2l, name: "company2", registeredAt: date("2010-10-10")),
-				new CompanyImpl(id: 3l, name: "company3"),
-				new CompanyImpl(id: 4l, name: null),
-				new CompanyImpl(id: 5l, name: "company", registeredAt: date("2001-01-01")),
-				new CompanyImpl(id: 6l, name: "company", registeredAt: date("2001-02-02"))
+				new CompanyImpl(id: 1, name: "company1"),
+				new CompanyImpl(id: 2, name: "company2", registeredAt: date("2010-10-10")),
+				new CompanyImpl(id: 3, name: "company3"),
+				new CompanyImpl(id: 4, name: null),
+				new CompanyImpl(id: 5, name: "company", registeredAt: date("2001-01-01")),
+				new CompanyImpl(id: 6, name: "company", registeredAt: date("2001-02-02"))
 		])
 
 		repository = new ObjectifyRepository(new ObjectifyExecutor(objectifyFactory, new ObjectifyOpts()), [
@@ -51,53 +51,186 @@ class ObjectifyRepositoryFindSpec extends Specification {
 
 	def "all criteria should find all entities"() {
 		when:
-		def actual = toIds(repository.find(entityClass, criteria, Pagination.ALL));
+		def actual = toIds(repository.find(entityClass, criteria, Pagination.ALL))
 		then:
 		actual == expected
 
 		where:
 		entityClass   | criteria       | expected
-		Company.class | company$.all() | [1l, 2l, 3l, 4l, 5l, 6l]
+		Company.class | company$.all() | [1, 2, 3, 4, 5, 6]
 	}
 
 
-	def "equals property criteria should find only entities with property value equal to specified"() {
+	def "equal property criteria should find only entities with property value equal to specified"() {
 		when:
-		def actual = toIds(repository.find(entityClass, criteria, Pagination.ALL));
+		def actual = toIds(repository.find(entityClass, criteria, Pagination.ALL))
+		then:
+		actual == expected
+
+		where:
+		entityClass   | criteria                                         | expected
+		Company.class | company$.id$.equal(1)                            | [1]
+		Company.class | company$.id$.eq(4)                               | [4]
+		Company.class | company$.id$.eq(10)                              | []
+		Company.class | company$.name$.equal("company")                  | [5, 6]
+		Company.class | company$.name$.eq("company1")                    | [1]
+		Company.class | company$.name$.eq("company8")                    | []
+		Company.class | company$.name$.eq(null)                          | [4]
+		Company.class | company$.registeredAt$.equal(date("2001-01-01")) | [5]
+		Company.class | company$.registeredAt$.eq(date("2001-01-02"))    | []
+		Company.class | company$.registeredAt$.eq(null)                  | [1, 3, 4]
+	}
+
+	def "not equal property criteria should find only entities with property value not equal to specified"() {
+		when:
+		def actual = toIds(repository.find(entityClass, criteria, Pagination.ALL))
 		then:
 		actual == expected
 
 		where:
 		entityClass   | criteria                                      | expected
-		Company.class | company$.id$.eq(1l)                           | [1l]
-		Company.class | company$.id$.eq(4l)                           | [4l]
-		Company.class | company$.id$.eq(10l)                          | []
-		Company.class | company$.name$.eq("company")                  | [5l, 6l]
-		Company.class | company$.name$.eq("company1")                 | [1l]
-		Company.class | company$.name$.eq("company8")                 | []
-		Company.class | company$.name$.eq(null)                       | [4l]
-		Company.class | company$.registeredAt$.eq(date("2001-01-01")) | [5l]
-		Company.class | company$.registeredAt$.eq(date("2001-01-02")) | []
-		Company.class | company$.registeredAt$.eq(null)               | [1l, 3l, 4l]
+		Company.class | company$.id$.notEqual(1)                      | [2, 3, 4, 5, 6]
+		Company.class | company$.id$.ne(10)                           | [1, 2, 3, 4, 5, 6]
+		Company.class | company$.name$.notEqual("company")            | [1, 2, 3, 4]
+		Company.class | company$.name$.ne("company1")                 | [2, 3, 4, 5, 6]
+		Company.class | company$.name$.ne("company8")                 | [1, 2, 3, 4, 5, 6]
+		Company.class | company$.name$.ne(null)                       | [1, 2, 3, 5, 6]
+		Company.class | company$.registeredAt$.ne(date("2001-01-01")) | [1, 2, 3, 4, 6]
+		Company.class | company$.registeredAt$.ne(date("2001-01-02")) | [1, 2, 3, 4, 5, 6]
+		Company.class | company$.registeredAt$.notEqual(null)         | [2, 5, 6]
 	}
 
-	def "not equals property criteria should find only entities with property value not equal to specified"() {
+	def "is null property criteria should find only entities with property value equal to null"() {
 		when:
-		def actual = toIds(repository.find(entityClass, criteria, Pagination.ALL));
+		def actual = toIds(repository.find(entityClass, criteria, Pagination.ALL))
 		then:
 		actual == expected
 
 		where:
-		entityClass   | criteria                                      | expected
-		Company.class | company$.id$.ne(1l)                           | [2l, 3l, 4l, 5l, 6l]
-		Company.class | company$.id$.ne(10l)                          | [1l, 2l, 3l, 4l, 5l, 6l]
-		Company.class | company$.name$.ne("company")                  | [1l, 2l, 3l, 4l]
-		Company.class | company$.name$.ne("company1")                 | [2l, 3l, 4l, 5l, 6l]
-		Company.class | company$.name$.ne("company8")                 | [1l, 2l, 3l, 4l, 5l, 6l]
-		Company.class | company$.name$.ne(null)                       | [1l, 2l, 3l, 5l, 6l]
-		Company.class | company$.registeredAt$.ne(date("2001-01-01")) | [1l, 2l, 3l, 4l, 6l]
-		Company.class | company$.registeredAt$.ne(date("2001-01-02")) | [1l, 2l, 3l, 4l, 5l, 6l]
-		Company.class | company$.registeredAt$.ne(null)               | [2l, 5l, 6l]
+		entityClass   | criteria                        | expected
+		Company.class | company$.name$.isNull()         | [4]
+		Company.class | company$.name$.eq(null)         | [4]
+		Company.class | company$.registeredAt$.isNull() | [1, 3, 4]
+		Company.class | company$.registeredAt$.eq(null) | [1, 3, 4]
+	}
+
+	def "is not null property criteria should find only entities with property value not equal to null"() {
+		when:
+		def actual = toIds(repository.find(entityClass, criteria, Pagination.ALL))
+		then:
+		actual == expected
+
+		where:
+		entityClass   | criteria                         | expected
+		Company.class | company$.name$.notNull()         | [1, 2, 3, 5, 6]
+		Company.class | company$.name$.ne(null)          | [1, 2, 3, 5, 6]
+		Company.class | company$.registeredAt$.notNull() | [2, 5, 6]
+		Company.class | company$.registeredAt$.ne(null)  | [2, 5, 6]
+	}
+
+	def "greater than property criteria should find only entities with property value greater than specified"() {
+		when:
+		def actual = toIds(repository.find(entityClass, criteria, Pagination.ALL))
+		then:
+		actual == expected
+
+		where:
+		entityClass   | criteria                                               | expected
+		Company.class | company$.id$.greaterThan(4)                            | [5, 6]
+		Company.class | company$.id$.gt(6)                                     | []
+		Company.class | company$.id$.gt(10)                                    | []
+		Company.class | company$.name$.greaterThan("company")                  | [1, 2, 3]
+		Company.class | company$.name$.gt("a")                                 | [1, 2, 3, 5, 6]
+		Company.class | company$.name$.gt("z")                                 | []
+		Company.class | company$.name$.gt(null)                                | [1, 2, 3, 5, 6]
+		Company.class | company$.registeredAt$.greaterThan(date("2001-01-01")) | [2, 6]
+		Company.class | company$.registeredAt$.gt(date("2010-10-10"))          | []
+		Company.class | company$.registeredAt$.gt(date("2012-10-10"))          | []
+		Company.class | company$.registeredAt$.gt(null)                        | [2, 5, 6]
+	}
+
+	def "greater or equal property criteria should find only entities with property value greater or equal to specified"() {
+		when:
+		def actual = toIds(repository.find(entityClass, criteria, Pagination.ALL))
+		then:
+		actual == expected
+
+		where:
+		entityClass   | criteria                                                  | expected
+		Company.class | company$.id$.greaterOrEqual(4)                            | [4, 5, 6]
+		Company.class | company$.id$.ge(6)                                        | [6]
+		Company.class | company$.id$.ge(10)                                       | []
+		Company.class | company$.name$.greaterOrEqual("company")                  | [1, 2, 3, 5, 6]
+		Company.class | company$.name$.ge("a")                                    | [1, 2, 3, 5, 6]
+		Company.class | company$.name$.ge("z")                                    | []
+		Company.class | company$.name$.ge(null)                                   | [1, 2, 3, 4, 5, 6]
+		Company.class | company$.registeredAt$.greaterOrEqual(date("2001-01-01")) | [2, 5, 6]
+		Company.class | company$.registeredAt$.ge(date("2010-10-10"))             | [2]
+		Company.class | company$.registeredAt$.ge(date("2012-10-10"))             | []
+		Company.class | company$.registeredAt$.ge(null)                           | [1, 2, 3, 4, 5, 6]
+	}
+
+	def "less than property criteria should find only entities with property value less than specified"() {
+		when:
+		def actual = toIds(repository.find(entityClass, criteria, Pagination.ALL))
+		then:
+		actual == expected
+
+		where:
+		entityClass   | criteria                                            | expected
+		Company.class | company$.id$.lessThan(4)                            | [1, 2, 3]
+		Company.class | company$.id$.lt(1)                                  | []
+		Company.class | company$.id$.lt(10)                                 | [1, 2, 3, 4, 5, 6]
+		Company.class | company$.name$.lessThan("company")                  | [4]
+		Company.class | company$.name$.lt("a")                              | [4]
+		Company.class | company$.name$.lt("z")                              | [1, 2, 3, 4, 5, 6]
+		Company.class | company$.name$.lt(null)                             | []
+		Company.class | company$.registeredAt$.lessThan(date("2001-01-01")) | [1, 3, 4]
+		Company.class | company$.registeredAt$.lt(date("2010-10-10"))       | [1, 3, 4, 5, 6]
+		Company.class | company$.registeredAt$.lt(date("2012-10-10"))       | [1, 2, 3, 4, 5, 6]
+		Company.class | company$.registeredAt$.lt(null)                     | []
+	}
+
+	def "less or equal property criteria should find only entities with property value less or equal to specified"() {
+		when:
+		def actual = toIds(repository.find(entityClass, criteria, Pagination.ALL))
+		then:
+		actual == expected
+
+		where:
+		entityClass   | criteria                                               | expected
+		Company.class | company$.id$.lessOrEqual(4)                            | [1, 2, 3, 4]
+		Company.class | company$.id$.le(1)                                     | [1]
+		Company.class | company$.id$.le(10)                                    | [1, 2, 3, 4, 5, 6]
+		Company.class | company$.name$.lessOrEqual("company")                  | [4, 5, 6]
+		Company.class | company$.name$.le("a")                                 | [4]
+		Company.class | company$.name$.le("z")                                 | [1, 2, 3, 4, 5, 6]
+		Company.class | company$.name$.le(null)                                | [4]
+		Company.class | company$.registeredAt$.lessOrEqual(date("2001-01-01")) | [1, 3, 4, 5]
+		Company.class | company$.registeredAt$.le(date("2010-10-10"))          | [1, 2, 3, 4, 5, 6]
+		Company.class | company$.registeredAt$.le(date("2012-10-10"))          | [1, 2, 3, 4, 5, 6]
+		Company.class | company$.registeredAt$.le(null)                        | [1, 3, 4]
+	}
+
+
+	def "property criteria should throw exception if it is used with key column and null value"() {
+		when:
+		repository.find(entityClass, criteria, Pagination.ALL)
+		then:
+		thrown(NullPointerException.class)
+
+		where:
+		entityClass   | criteria
+		Company.class | company$.id$.isNull()
+		Company.class | company$.id$.eq(null)
+		Company.class | company$.id$.notNull()
+		Company.class | company$.id$.ne(null)
+		Company.class | company$.id$.gt(null)
+		Company.class | company$.id$.ge(null)
+		Company.class | company$.id$.lt(null)
+		Company.class | company$.id$.le(null)
+		Company.class | company$.id$.like(null)
+		Company.class | company$.id$.in([null])
 	}
 
 	private Date date(final String input) {
