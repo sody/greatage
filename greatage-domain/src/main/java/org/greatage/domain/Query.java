4c46e46009ed8d8c3fae57181578962a9fbb2523
Ivan Khalopik <ikhalopik@gmail.com>
2013-04-27 13:21:56 +0300

    issue #6 Domain API
    query api enhancements

--- greatage-domain/src/main/java/org/greatage/domain/Query.java
+++ greatage-domain/src/main/java/org/greatage/domain/Query.java
@@ -11,20 +11,47 @@ public interface Query<PK extends Serializable, E extends Entity<PK>> {
 
     Query<PK, E> filter(Criteria<PK, E> criteria);
 
+    Query<PK, E> filter(String filter, Object value);
+
+    Query<PK, E> and();
+
+    Query<PK, E> or();
+
+    Query<PK, E> end();
+
+
     Query<PK, E> fetch(Property property);
 
+    Query<PK, E> fetch(Property property, boolean fetch);
+
+
+    Query<PK, E> sort(Property property);
+
+    Query<PK, E> sort(Property property, boolean ascending);
+
     Query<PK, E> sort(Property property, boolean ascending, boolean ignoreCase);
+
+
+    Query<PK, E> skip(int count);
+
+    Query<PK, E> limit(int count);
 
     Query<PK, E> paginate(int start, int count);
 
 
     long count();
 
+    E unique();
+
+    E first();
+
     List<E> list();
+
+    Iterable<E> iterate();
 
     List<PK> keys();
 
-    E unique();
+    Iterable<PK> iterateKeys();
 
     interface Property {
 
--- greatage-domain/src/main/java/org/greatage/domain/internal/AbstractQuery.java
+++ greatage-domain/src/main/java/org/greatage/domain/internal/AbstractQuery.java
@@ -12,99 +12,154 @@ import java.util.List;
  * @since 1.0
  */
 public abstract class AbstractQuery<PK extends Serializable, E extends Entity<PK>>
-		implements Query<PK, E> {
+        implements Query<PK, E> {
 
-	private final Class<E> entityClass;
+    private final Class<E> entityClass;
 
-	private Criteria<PK, E> criteria;
-	private List<Property> fetches;
-	private List<Sort> sorts;
+    private Criteria<PK, E> criteria;
+    private List<Property> fetches;
+    private List<Sort> sorts;
 
-	private int start = 0;
-	private int count = -1;
+    private int start = 0;
+    private int count = -1;
 
-	protected AbstractQuery(final Class<E> entityClass) {
-		this.entityClass = entityClass;
-	}
+    protected AbstractQuery(final Class<E> entityClass) {
+        this.entityClass = entityClass;
+    }
 
-	public Query<PK, E> filter(final Criteria<PK, E> criteria) {
-		this.criteria = this.criteria != null ?
-				this.criteria.and(criteria) :
-				criteria;
+    public Query<PK, E> filter(final Criteria<PK, E> criteria) {
+        this.criteria = this.criteria != null ?
+                this.criteria.and(criteria) :
+                criteria;
 
-		return this;
-	}
+        return this;
+    }
 
-	public Query<PK, E> fetch(final Property property) {
-		if (fetches == null) {
-			fetches = new ArrayList<Property>();
-		}
-		fetches.add(property);
+    public Query<PK, E> filter(final String filter, final Object value) {
+        //TODO: implement this
+        throw new UnsupportedOperationException();
+    }
 
-		return this;
-	}
+    public Query<PK, E> and() {
+        //TODO: implement this
+        throw new UnsupportedOperationException();
+    }
 
-	public Query<PK, E> sort(final Property property, final boolean ascending, final boolean ignoreCase) {
-		if (sorts == null) {
-			sorts = new ArrayList<Sort>();
-		}
-		sorts.add(new Sort(property, ascending, ignoreCase));
+    public Query<PK, E> or() {
+        //TODO: implement this
+        throw new UnsupportedOperationException();
+    }
 
-		return this;
-	}
+    public Query<PK, E> end() {
+        //TODO: implement this
+        throw new UnsupportedOperationException();
+    }
 
-	public Query<PK, E> paginate(final int start, final int count) {
-		this.start = start;
-		this.count = count;
+    public Query<PK, E> fetch(final Property property) {
+        if (fetches == null) {
+            fetches = new ArrayList<Property>();
+        }
+        fetches.add(property);
+        return this;
+    }
 
-		return this;
-	}
+    public Query<PK, E> fetch(final Property property, final boolean fetch) {
+        //TODO: implement this logic
+        return fetch(property);
+    }
 
-	public Class<E> getEntityClass() {
-		return entityClass;
-	}
+    public Query<PK, E> sort(final Property property) {
+        return sort(property, true);
+    }
 
-	public Criteria<PK, E> getCriteria() {
-		return criteria;
-	}
+    public Query<PK, E> sort(final Property property, final boolean ascending) {
+        return sort(property, ascending, false);
+    }
 
-	public List<Property> getFetches() {
-		return fetches;
-	}
+    public Query<PK, E> sort(final Property property, final boolean ascending, final boolean ignoreCase) {
+        if (sorts == null) {
+            sorts = new ArrayList<Sort>();
+        }
+        sorts.add(new Sort(property, ascending, ignoreCase));
+        return this;
+    }
 
-	public List<Sort> getSorts() {
-		return sorts;
-	}
+    public Query<PK, E> skip(final int count) {
+        this.start = count;
+        return this;
+    }
 
-	public int getStart() {
-		return start;
-	}
+    public Query<PK, E> limit(final int count) {
+        this.count = count;
+        return this;
+    }
 
-	public int getCount() {
-		return count;
-	}
+    public Query<PK, E> paginate(final int start, final int count) {
+        this.start = start;
+        this.count = count;
+        return this;
+    }
 
-	class Sort {
-		private final Property property;
-		private final boolean ascending;
-		private final boolean ignoreCase;
+    public E first() {
+        //TODO: implement this
+        throw new UnsupportedOperationException();
+    }
 
-		Sort(final Property property, final boolean ascending, final boolean ignoreCase) {
-			this.property = property;
-			this.ascending = ascending;
-			this.ignoreCase = ignoreCase;
-		}
+    public Iterable<E> iterate() {
+        //TODO: implement this
+        throw new UnsupportedOperationException();
+    }
 
-		public Property getProperty() {
-			return property;
-		}
+    public Iterable<PK> iterateKeys() {
+        //TODO: implement this
+        throw new UnsupportedOperationException();
+    }
 
-		public boolean isAscending() {
-			return ascending;
-		}
+    public Class<E> getEntityClass() {
+        return entityClass;
+    }
 
-		public boolean isIgnoreCase() {
-			return ignoreCase;
-		}
-	}
+    public Criteria<PK, E> getCriteria() {
+        return criteria;
+    }
+
+    public List<Property> getFetches() {
+        return fetches;
+    }
+
+    public List<Sort> getSorts() {
+        return sorts;
+    }
+
+    public int getStart() {
+        return start;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    class Sort {
+        private final Property property;
+        private final boolean ascending;
+        private final boolean ignoreCase;
+
+        Sort(final Property property, final boolean ascending, final boolean ignoreCase) {
+            this.property = property;
+            this.ascending = ascending;
+            this.ignoreCase = ignoreCase;
+        }
+
+        public Property getProperty() {
+            return property;
+        }
+
+        public boolean isAscending() {
+            return ascending;
+        }
+
+        public boolean isIgnoreCase() {
+            return ignoreCase;
+        }
+    }
 }
